44adde325ec71d24bb26ab2756dcae7c331d1673
anuta777 <anna.pavlikhina@mail.ru>
2023-08-07 13:30:52 +0300

    решение 23 задачи

--- DZ023.cs/DZ023.cs
+++ DZ023.cs/DZ023.cs
@@ -1,1 +1,32 @@
 # Задача № 23
+
+// Напиши программу ,которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N/
+Console.Write("Введите число (N)");
+
+void CubeNumber(int number)
+
+{
+
+if (number < 0)
+{
+    Console.Write("Ввели отрицательное число");
+
+    number = number * -1;
+}
+
+int count =1;
+
+while (count <=number)
+{
+    int result = count*count*count;
+    Console.Write(result + "");
+    count ++;
+}
+
+}
+int number = Convert.ToInt32(Console.ReadLine());
+
+  CubeNumber(number);
+
+Console.WriteLine();
+
